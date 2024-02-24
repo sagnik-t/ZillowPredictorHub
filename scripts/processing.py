09766ec5c25b5670f3c26f9e0f2b29a03117cda0
Sagnik <sagniktaraphdar@gmail.com>
2024-02-24 18:00:30 +0530

    Generate clean data for median_price_allHomes

--- scripts/processing.py
+++ scripts/processing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('../data/raw/City_MedianRentalPrice_AllHomes.csv', index_col=0)
+df = pd.read_csv('./data/raw/City_MedianRentalPrice_AllHomes.csv', index_col=0)
 df.drop(columns=['SizeRank'], inplace=True)
 df_long = pd.melt(df, id_vars=df.columns[:4], var_name='timestamp', value_name='median_price')
-df_long.to_csv('../data/processed/median_price_all_homes.csv')
+df_long.to_csv('./data/processed/median_price_all_homes.csv')
