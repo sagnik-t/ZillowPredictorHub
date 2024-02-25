6e9858308184536c7a2c51039050a8f3fa038d27
Sagnik <sagniktaraphdar@gmail.com>
2024-02-25 12:39:55 +0530

    Add Config class to config/config.py file

--- config/config.py
+++ config/config.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 class Config:
-    ROOT_PATH = Path(__file__).resolve().parent
-    RAW_DATA_PATH = ROOT_PATH / 'data/raw'
-    PROCESSED_DATA_PATH = ROOT_PATH / 'data/processed'
+    ROOT_PATH = Path(__file__).parent.parent
+    RAW_DATA_PATH = ROOT_PATH / 'data' / 'raw'
+    PROCESSED_DATA_PATH = ROOT_PATH / 'data' / 'processed'
+    
