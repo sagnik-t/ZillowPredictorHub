93254c3fe726e8eca78e0b1c867bdf6719ed6ed9
Sagnik <sagniktaraphdar@gmail.com>
2024-02-25 12:45:01 +0530

    Add direct file paths to config file

--- config/config.py
+++ config/config.py
@@ -4,4 +4,5 @@ class Config:
     ROOT_PATH = Path(__file__).parent.parent
     RAW_DATA_PATH = ROOT_PATH / 'data' / 'raw'
     PROCESSED_DATA_PATH = ROOT_PATH / 'data' / 'processed'
-    
+    RAW_DATA_FILE = RAW_DATA_PATH / 'City_MedianRentalPrice_AllHomes.csv'
+    PROCESSED_DATA_FILE = PROCESSED_DATA_PATH / 'City_MedianRentalPrice_AllHomes.csv'
--- scripts/melt_data.py
+++ scripts/melt_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from config.config import Config
 
-df = pd.read_csv(Config.RAW_DATA_PATH / 'City_MedianRentalPrice_AllHomes.csv',  index_col=0)
+df = pd.read_csv(Config.RAW_DATA_FILE, index_col=0)
 df.drop(columns=['SizeRank'], inplace=True)
 df_long = pd.melt(df, id_vars=df.columns[:4], var_name='timestamp', value_name='median_price')
 df_long.to_csv('./data/processed/median_price_all_homes2.csv')
